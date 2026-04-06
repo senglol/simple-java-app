40a8c3d9f5a9dc141cac756acda89813a0c54de6
BS Beh <boon-seng.beh@keysight.com>
2026-04-06 16:49:45 +0800

    Some changes to test webhooks 1

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("magic lai lai");
+        System.out.println("magic lai lai lai");
     }
 
     public String getMessage() {
