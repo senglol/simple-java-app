73f135b8732bf18b24d01ff886f3d1beaf8fbfaa
BS Beh <boon-seng.beh@keysight.com>
2026-04-07 14:38:51 +0800

    Make a change to pipeline job to demonstrate webhook

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("magic lai lai lai 123");
+        System.out.println("magic lai lai lai 321");
     }
 
     public String getMessage() {
