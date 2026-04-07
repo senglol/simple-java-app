675b2beeff3fd650d5fb101ba58c23305dcd89a5
BS Beh <boon-seng.beh@keysight.com>
2026-04-07 14:21:21 +0800

    Make a change to pipeline job to demonstrate Poll SCM

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("magic lai lai lai");
+        System.out.println("magic lai lai lai 123");
     }
 
     public String getMessage() {
